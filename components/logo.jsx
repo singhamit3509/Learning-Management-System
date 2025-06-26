dfe115efe0883707863898a6adfcb61b457df476
AMIT SINGH <amitsingh@AMITs-MacBook-Air.local>
2025-06-26 21:05:59 +0530

    main dashboard is completed

--- components/logo.jsx
+++ components/logo.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import logo from "@/assets/astronLMS.png";
 import { cn } from "@/lib/utils";
 
+
 const Logo = ({ className = "" }) => {
   return (
     <div className="flex items-center  justify-center md:pl-20">
@@ -13,9 +14,9 @@ const Logo = ({ className = "" }) => {
           alt="logo"
         />
       </div>
-      <div className="flex flex-col items-center justify-center mt-1">
-        <h5 className="text-[#67ac4d] text-[20px] leading-5 font-bold">Learning</h5>
-        <h5 className="text-[#9ac34d] text-[8px] font-medium">Management System</h5>
+      <div className="flex flex-col items-center justify-center mt-13">
+        <h5 className="text-[#67ac4d] text-[18px] leading-4 font-bold">Learning</h5>
+        <h5 className="text-[#9ac34d] text-[6px] font-medium">Management System</h5>
       </div>
     </div>
   );
